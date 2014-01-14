51b80b78ffb52e649a05822f451a45046b1184b8
kor-ka <badgrib@gmail.com>
2014-01-14 15:13:19 +0400

    init

--- src/com/mycompany/myapp/MainActivity.java
+++ src/com/mycompany/myapp/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import android.content.pm.*;
+import java.util.*;
+import android.content.pm.PackageManager.*;
 
 public class MainActivity extends Activity
 {
@@ -13,5 +16,25 @@ public class MainActivity extends Activity
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+	List<ApplicationInfo> packages =	getPackageManager().getInstalledApplications(PackageManager.GET_META_DATA);
+		
+	for(ApplicationInfo packageInfo : packages){
+		String packageName = packageInfo.packageName;
+		
+		try
+		{
+			PackageInfo pkgInfo = getPackageManager().getPackageInfo(
+				packageName, 
+				PackageManager.GET_PERMISSIONS
+			);
+			PermissionInfo[] permissions = pkgInfo.permissions;
+			for (PermissionInfo permission : permissions){
+				permission.toString();
+			}
+		}
+		catch (PackageManager.NameNotFoundException e)
+		{}
+		
+	}
     }
 }
